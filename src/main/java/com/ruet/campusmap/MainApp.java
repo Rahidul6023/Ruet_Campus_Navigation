4505d23408c07d2135a7777ec42ef5142798e996
Rafimahmud-24 <rafimahmud467@gmail.com>
2026-09-09 09:49:24 +0600

    Implemented Basic UI

--- src/main/java/com/ruet/campusmap/MainApp.java
+++ src/main/java/com/ruet/campusmap/MainApp.java
@@ -1,23 +1,27 @@
-package com.ruet.campusmap;
+package com.ruet.nav;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class MainApp extends Application {
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/ruet/nav/main-view.fxml"));
+        Scene scene = new Scene(loader.load(), 1100, 750);
+        scene.getStylesheets().add(
+                Objects.requireNonNull(getClass().getResource("/com/ruet/nav/style.css")).toExternalForm());
 
-        Label label = new Label("RUET Campus Map");
-        StackPane root = new StackPane(label);
-
-        Scene scene = new Scene(root, 1200, 800);
-
-        stage.setTitle("RUET Campus Map");
+        stage.setTitle("RUET Navigation System");
         stage.setScene(scene);
+        stage.setMinWidth(900);
+        stage.setMinHeight(600);
         stage.show();
     }
 
